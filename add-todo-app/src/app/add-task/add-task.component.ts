08aec19a1d10ba5c6735e3ef89da0f5ed2b0df7a
Estherexcel <ogokunleesther@gmail.com>
2024-01-16 18:54:24 +0100

    first logic, object creation and constructor

--- add-todo-app/src/app/add-task/add-task.component.ts
+++ add-todo-app/src/app/add-task/add-task.component.ts
@@ -3,15 +3,27 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Task } from '../model/task';
 
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, FormsModule, CommonModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
+  taskName: string;
+  dueDate: string;
+  tags: string;
+
+  constructor (){
+    this.taskName = '';
+    this.dueDate = '';
+    this.tags = '';
+  }
 
 }
