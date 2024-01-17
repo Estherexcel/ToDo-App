14c42e401d7ae734c1e4febee7a217f9ac4d8eb4
Estherexcel <ogokunleesther@gmail.com>
2024-01-17 19:49:27 +0100

    add and display task functionalities updated

--- add-todo-app/src/app/add-task/add-task.component.ts
+++ add-todo-app/src/app/add-task/add-task.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { log } from 'console';
+import { Task } from '../model/task';
 
 
 @Component({
@@ -16,29 +16,21 @@ import { log } from 'console';
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
-  taskObj: Task;
+  taskObj: Task = {
+    name: '',
+    tags: [],
+    date: new Date()
+  };
+
+
   taskList: Task[] = []
-createNewTask(){
-  console.log(this.taskObj)
-  
-  this.taskList.push(this.taskObj)   // adding a new task to the tasklist
+  createNewTask() {
+    const task = JSON.stringify(this.taskObj)
+    const parse = JSON.stringify(task)
+    this.taskList.push(this.taskObj)   // adding a new task to the tasklist
     localStorage.setItem('addTask', JSON.stringify(this.taskList))   // to save list in local storage
-console.log(this.taskList)
-}
-  constructor (){
-    this.taskObj = new Task()
+    console.log(this.taskList)
   }
 
 }
 
-export class Task{
-  taskName: string;
-  dueDate: string;
-  tags: string;
-
-  constructor (){
-    this.taskName = '';
-    this.dueDate = '';
-    this.tags = '';
-  }
-}
--- add-todo-app/src/app/model/task.ts
+++ add-todo-app/src/app/model/task.ts
@@ -1,5 +1,4 @@
 export interface Task {
-    id: number;
     name: string;
     date: Date;
     tags: string[];
